7ecf9d7ab54004237fee2bdc13414b8bc1021fab
jesse <tomilayojesse@gmail.com>
2022-10-18 12:30:18 +0100

    commit

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -26,7 +26,7 @@
     <ion-list> 
     
       <ion-item>
-        <ion-icon slot="start" color="danger" :icon="arrowUp"></ion-icon>
+        <ion-icon size="small" slot="start" color="danger" :icon="arrowUp"></ion-icon>
         <ion-label  class="list">
         UPGRADE ACCOUNT
         </ion-label>
@@ -34,7 +34,7 @@
       </ion-item>
       <br>
       <ion-item class="list">
-        <ion-avatar slot="start">
+        <ion-avatar size="small" slot="start">
         <img src="../assets/icons/4.png" alt="">
         
         </ion-avatar>
@@ -45,7 +45,7 @@
       </ion-item >
       <br>
       <ion-item class="list">
-        <ion-icon color="tertiary" slot="start" :icon="documentText"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="documentText"></ion-icon>
         <ion-label>
           REQUEST STATEMENT
         </ion-label>
@@ -53,7 +53,7 @@
       </ion-item>
     <br>
       <ion-item>
-        <ion-icon color="tertiary" slot="start" :icon="chatboxEllipses"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="chatboxEllipses"></ion-icon>
         <ion-label>
           SUPPORT
         </ion-label>
@@ -61,7 +61,7 @@
       </ion-item>
       <br>
       <ion-item>
-        <ion-icon color="tertiary" slot="start" :icon="card"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="card"></ion-icon>
         <ion-label>
           CARDS & BANKS
         </ion-label>
@@ -69,7 +69,7 @@
       </ion-item>
       <br>
       <ion-item>
-        <ion-icon color="tertiary" slot="start" :icon="lockClosed"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="lockClosed"></ion-icon>
         <ion-label>
           SECURITY SETTINGS
         </ion-label>
@@ -77,7 +77,7 @@
       </ion-item>
       <br>
       <ion-item>
-        <ion-icon color="tertiary" slot="start" :icon="cog"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="cog"></ion-icon>
         <ion-label>
           PREFERENCES
         </ion-label>
@@ -85,7 +85,7 @@
       </ion-item>
       <br>
       <ion-item>
-        <ion-icon color="tertiary" slot="start" :icon="unlink"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="unlink"></ion-icon>
         <ion-label>
           ACCOUNT LIMITS
         </ion-label>
@@ -93,7 +93,7 @@
       </ion-item>
       <br>
       <ion-item>
-        <ion-icon color="tertiary" slot="start" :icon="shareSocial"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="shareSocial"></ion-icon>
         <ion-label>
           REFER A FRIEND
         </ion-label>
@@ -101,7 +101,7 @@
       </ion-item>
       <br>
       <ion-item>
-        <ion-icon color="warning" slot="start" :icon="star"></ion-icon>
+        <ion-icon size="small" color="warning" slot="start" :icon="star"></ion-icon>
         <ion-label>
           RATE US
         </ion-label>
@@ -109,11 +109,11 @@
       </ion-item>
       <br>
       <ion-item>
-        <ion-icon color="tertiary" slot="start" :icon="square"></ion-icon>
+        <ion-icon size="small" color="tertiary" slot="start" :icon="square"></ion-icon>
         <ion-label>
           ABOUT US
         </ion-label>
-        <ion-icon size="small" slot="end" :icon="chevronForward"></ion-icon>
+        <ion-icon  size="small" slot="end" :icon="chevronForward"></ion-icon>
       </ion-item>
     </ion-list>
      </ion-content>
